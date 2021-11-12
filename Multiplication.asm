f6d4ceb2ddc145c1024f7eff6eda71c373a28b0e
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2021-11-12 21:22:50 +0500

    Update Multiplication.asm

--- Multiplication.asm
+++ Multiplication.asm
@@ -1,12 +1,10 @@
-                  ;Print Function  
-;x---------------------------------------------x 
+                  ;Print Function   
  print macro p1
 mov dl,p1   
 mov ah,02
 int 21h  
  endm     
         ;Output Function  
-;x---------------------------------------------x
 Output macro p1
 mov dx,offset p1   
 mov ah,09
@@ -15,28 +13,24 @@ endm
 
 
 ;program to find Multiplication of Two Numbers
-;x---------------------------------------------x 
+ 
 .model small
 .stack 100h
 .data 
 
- msg db " ENTER FIRST VALUE---->$"    
- var db 0ah, " ENTER SECOND VALUE---->$"
- var1 db 0ah,"  RESULT----->$" 
+ msg db " ENTER FIRST VALUE==>$"    
+ msg1 db 0ah, " ENTER SECOND VALUE==>$"
+ var1 db 0ah,"  RESULT==>$" 
  newLine db 0ah,0dh,"$"
 .code 
 main proc 
     
-                 ;first  step
+                 
        
-   ;take first value from the user and print 
-   ;first value
-;x----------------------------------------------x        
+   ;take first value from the user         
     mov ax,@data
 mov ds,ax
-mov dx,offset msg
-mov ah,09h 
- int 21h  
+Output msg  
        
        mov ah,01h
        int 21h
@@ -44,64 +38,50 @@ mov ah,09h
        mov bl,al
       sub bl,48
        
-              ;new line logic
-             ;x--------------x    
              
-      Output newline
-       
-             ;second  step 
-  
-;take second value from the user and print
-;second value  
-
-;x------------------------------------------x
-                
-      Output msg
-       
-       mov ah,01h
-       int 21h
-       sub al,48 
+             
+Output newline
+;take second value from the user 
+Output msg1
+mov ah,01h
+int 21h
+sub al,48 
         
            ;multiplication logic
-         ;x----------------------x
-          
-       mul bl        
-       aam
         
-       mov bl,al       
-     add bl,48
-              
-      mov ch,ah       
-     add ch,48
+          
+mul bl        
+aam
+mov bl,al       
+add bl,48
+mov ch,ah       
+add ch,48
                 ;new line logic
-               ;x--------------x
                
-     Output  newline
+               
+Output  newline
      
      
-                 ;final  step 
-   
+                 
                  ;result print
-   ;x---------------------------------------x
+   
                     
-   Output var1
+Output var1
      
      
     
 
-     mov dl,ch
-     mov ah,02h
-     int 21h 
+mov dl,ch
+mov ah,02h
+int 21h 
      
-     
-      mov dl,bl
-     mov ah,02h
-     int 21h     
+mov dl,bl
+mov ah,02h
+int 21h     
 
       
            
 ;return statement 
-Output newline
 mov ah,4ch       
 int 21h 
     
